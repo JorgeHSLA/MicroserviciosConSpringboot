ce15ead9a31edc327c4313afcf6d77f4b0bd2254
junick25 <jorsie25@outlook.com>
2025-01-03 20:06:03 -0500

    se agregan post y put en el controlador de company

--- companies-crud/companies-crud/src/main/java/com/ejemploCurso/companies_crud/controllers/CompanyController.java
+++ companies-crud/companies-crud/src/main/java/com/ejemploCurso/companies_crud/controllers/CompanyController.java
@@ -6,9 +6,11 @@ import java.net.URI;
 import javax.swing.text.StyledEditorKit.BoldAction;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,10 +35,23 @@ public class CompanyController {
     }
 
     @PostMapping
-    ResponseEntity<Company> post(@RequestBody  Company company){
-        log.info("POST:company {}",company.getName()); 
+    ResponseEntity<Company> post(@RequestBody  Company company){ //request body es un json que se cambia (en este caso) a tipo Company
+         log.info("POST:company {}",company.getName()); 
         return ResponseEntity.created(URI.create(this.companyService.create(company).getName())).build(); // se cra la URI ej:/Google
 
     }
     
+    @PutMapping(path = "{name}") //Es una anotación de Spring que indica que este método maneja solicitudes HTTP del tipo GET.
+    ResponseEntity<Company> put(@RequestBody  Company company ,@PathVariable String name) throws NoSuchFieldException{ //Si alguien accede a /company/Google, el valor "Google" se pasa al parámetro name.
+        log.info("PUT:company {}",name); // mensaje que indica queu todo esta bien
+        return ResponseEntity.ok(this.companyService.update(company, name)); //Crea una respuesta HTTP con el estado 200 OK, lo que esta en el ok es lo que devuelve, en este caso una compania con el nombre name
+    }
+
+    @DeleteMapping(path = "{name}") 
+    ResponseEntity<?> delete(@PathVariable String name){//la interrogacion significa quese puede devolver cualquier objeto 
+
+        log.info("DELETE:company {}",name);
+        return ResponseEntity.noContent().build();
+    } 
+
 }
--- companies-crud/companies-crud/src/main/java/com/ejemploCurso/companies_crud/services/CompanyService.java
+++ companies-crud/companies-crud/src/main/java/com/ejemploCurso/companies_crud/services/CompanyService.java
@@ -4,7 +4,7 @@ import com.ejemploCurso.companies_crud.entities.Company;
 
 public interface CompanyService {
 
-        Company readByName(String name);
+        Company readByName(String name) throws NoSuchFieldException;
         Company create(Company company);
         Company update(Company company, String newName);
         void delete(String name);
