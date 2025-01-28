d8a67fb94567cf251afe33674fbf01c39338e904
junick25 <jorsie25@outlook.com>
2025-01-27 21:11:01 -0500

    SE AGREGA  EL SERVIDOR DE REGISTRY DESDE SPRINGBOOT

---
+++ registryServer/registryServer/src/main/java/com/debuggeandoIdeas/registryServer/RegistryServerApplication.java
@@ -0,0 +1,14 @@
+package com.debuggeandoIdeas.registryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServerApplication.class, args);
+	}
+
+}
